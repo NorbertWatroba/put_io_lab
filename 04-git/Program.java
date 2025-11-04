2ca83c5bb59cd52cbc9ada7c74154de1f4dc680d
Norbert Wątroba <norbertwatroba@norberts-macbook-pro-1.home>
2025-11-04 20:37:27 +0100

    1 zmiana

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,8 +1,10 @@
 // comment
 public class Main {
+  bool a = 1;
   int x = 8;
 
   public static void main(String[] args) {
+    a = 0;
     Main myObj = new Main();
     System.out.println(myObj.x/2);
     a = 1;
